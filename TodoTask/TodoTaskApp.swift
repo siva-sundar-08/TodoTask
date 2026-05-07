1c5c65512e5693d8ee8a68385650afd3b1cb0a7c
littlevibe-maker <sivasundar5944@gmail.com>
2026-05-07 19:57:49 +0530

    Initial Commit

---
+++ TodoTask/TodoTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTaskApp.swift
+//  TodoTask
+//
+//  Created by Siva Sundar on 2026-05-07.
+//
+
+import SwiftUI
+
+@main
+struct TodoTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
